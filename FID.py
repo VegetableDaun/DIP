fb54c402739fda90d7faa6c37efab252604cb5af
Kirill <kirillilenkov974@gmail.com>
2023-11-30 12:37:30 +0300

    changed param for new model

--- FID.py
+++ FID.py
@@ -1,24 +1,24 @@
-import os
 import json
+import os
+from pathlib import Path
+
 import numpy as np
 import tensorflow as tf
-
+from config_GAN import latent_dim, path_to_result, batch_size
 from scipy import linalg
-from tensorflow import keras
-from config_GAN import latent_dim
 
 
 class FID:
-    def __init__(self, data=None, step_gen=None):
+    def __init__(self, generator=None, data=None, step_gen=None):
         self.gen_mu = None
         self.gen_sigma = None
-        self.generator = None
+        self.generator = generator
 
         self.inception_model = tf.keras.applications.InceptionV3(include_top=False,
                                                                  weights="imagenet",
                                                                  pooling='avg')
 
-        self.path_to_result = 'real_mu_sigma.json'
+        self.path_to_result = Path(path_to_result) / 'real_mu_sigma.json'
         self.data = data
         self.step_gen = step_gen
 
@@ -27,6 +27,7 @@ class FID:
                 real_mu_sigma = list(json.load(F))
             self.real_mu = np.array(real_mu_sigma[0])
             self.real_sigma = np.array(real_mu_sigma[1])
+
         elif self.data is not None:
             real_embeddings = np.zeros([1, 2048])
             for i in self.data:
@@ -34,10 +35,10 @@ class FID:
                 i = i[0] * 255
 
                 # [optional]: we may need 3 channel (instead of 1)
-                i = np.repeat(i, 3, axis=-1)
+                # i = np.repeat(i, 3, axis=-1)
 
-                # resize the input shape , i.e. old shape: 28, new shape: 112
-                i = tf.image.resize(i, [112, 112])  # if we want to resize
+                # resize the input shape , i.e. old shape: 32, new shape: 112
+                i = tf.image.resize(i, [256, 256])  # if we want to resize
 
                 # round values
                 i = tf.round(i)
@@ -55,26 +56,26 @@ class FID:
 
     def calculate_fid(self, path_to_generator):
         # generate image
-        self.generator = keras.models.load_model(path_to_generator)
+        self.generator.load(path_to_generator)
 
         generated_embeddings = np.zeros([1, 2048])
         for i in self.data:
             # Create noise vector with label
-            random_latent_vectors = tf.random.normal(shape=(tf.shape(i[1])[0], latent_dim))
-            random_vector_labels = tf.concat([random_latent_vectors, i[1]], axis=1)
+            latent_vectors = tf.random.normal(shape=(batch_size, latent_dim))
+            vector_labels = i[1]
 
             # Generating the images
-            generated_img = self.generator.predict(random_vector_labels, verbose=0)
-            # Preparing generated_img for inceptionV3
+            generated_img = self.generator(latent_vectors, c=vector_labels)
 
-            # we need to have values between 1 and 255
+            # Preparing generated_img for inceptionV3
+            # We need to have values between 1 and 255
             generated_img = generated_img * 255
 
             # [optional]: we may need 3 channel (instead of 1)
-            generated_img = np.repeat(generated_img, 3, axis=-1)
+            # generated_img = np.repeat(generated_img, 3, axis=-1)
 
-            # resize the input shape , i.e. old shape: 28, new shape: 112
-            generated_img = tf.image.resize(generated_img, [112, 112])  # if we want to resize
+            # resize the input shape , i.e. old shape: 32, new shape: 112
+            generated_img = tf.image.resize(generated_img, [256, 256])  # if we want to resize
 
             # round values
             generated_img = tf.round(generated_img)
@@ -83,14 +84,13 @@ class FID:
             generated_img = tf.keras.applications.inception_v3.preprocess_input(generated_img)
 
             # inceptionV3 predicts
-            predicted_img = self.inception_model.predict(generated_img, verbose=0)
+            predicted_img = self.inception_model.predict(generated_img, verbose=1)
             generated_embeddings = np.vstack((generated_embeddings, predicted_img))
 
             if self.step_gen is None:
                 continue
             elif generated_embeddings.shape[0] > self.step_gen:
                 break
-
 
         # calculate mean and covariance statistics
         self.gen_mu, self.gen_sigma = generated_embeddings.mean(axis=0), np.cov(generated_embeddings, rowvar=False)
